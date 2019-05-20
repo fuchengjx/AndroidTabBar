47b2858e5409209c6fd5f2914bd510ed3317fb38
fc <327619852@qq.com>
2019-05-20 22:50:52 +0800

    圣杯布局

--- Android_views/Home.js
+++ Android_views/Home.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';  
-import {  
-  AppRegistry,  
-  StyleSheet,  
-  Text,  
-  View,  
-  Image,  
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
 } from 'react-native';
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      header: "我是Home的Header",
-      content: '我是首页',
-      footer: '我是Home的 底部'
-    } 
+      header: "我是Home的 Header",
+      content: '我是Home的 Content',
+      footer: '我是Home的 footer'
+    }
   }
   render() {
     return (
@@ -23,10 +23,18 @@ class Home extends Component {
           <Text>{this.state.header}</Text>
         </View>
         <View style={styles.content}>
-          <Text>{this.state.content}</Text>
+          <View style={styles.left}>
+            <Text>left</Text>
+          </View>
+          <View style={styles.center}>
+            <Text>{this.state.content}</Text>
+          </View>
+          <View style={styles.right}>
+            <Text>right</Text>
+          </View>
         </View>
-        <View style={styles.header}>
-          <Text>{this.state.footer}</Text>
+        <View style={styles.footer}>
+          <Text style={{textAlign: 'center'}}>{this.state.footer}</Text>
         </View>
       </View>
     )
@@ -36,19 +44,36 @@ const styles = StyleSheet.create({
   container: {
     display: 'flex',
     flexDirection: 'column',
-    height: 300
+    height: 300,
+    textAlign: 'center'
   },
   header: {
     flex: 1,
     backgroundColor: 'red'
+
   },
   content: {
     flex: 1,
+    display: 'flex',
+    flexDirection: 'row',
     backgroundColor: 'blue'
+  },
+  center: {
+    flex: 2,
+    backgroundColor: 'yellow'
+  },
+  left: {
+    flex: 1,
+    backgroundColor: 'blue'
+  },
+  right: {
+    flex: 1,
+    backgroundColor: 'green'
   },
   footer: {
     flex: 1,
-    backgroundColor: 'black'
+    backgroundColor: 'pink',
+
   }
 })
 // AppRegistry.registerComponent('Home', () => Home);
