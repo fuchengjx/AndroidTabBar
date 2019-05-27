366e9ed5a874c28a83d92236a4eeace87b6b2669
fc <327619852@qq.com>
2019-05-27 21:14:03 +0800

    tool class

---
+++ Android_views/util.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Dimensions, PixelRatio } from 'react-native';
+
+// fetch
+// 获取屏幕的宽高
+// 获去最小线宽
+
+
+module.exports = {
+  size: {
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+  },
+  pixel: 1 / PixelRatio.get(),
+
+  //fetch <=> ajax
+  get: function(url, successCallback, failCallback) {
+   fetch(url)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      successCallback(responseJson)
+    })
+    .catch((error) => {
+      failCallback(error)
+    });
+ }
+
+};
